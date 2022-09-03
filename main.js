f97fe227d4e6a45d3cafc1ee3805e69b8f0eb2fa
snezk456 <86143353+snezk456@users.noreply.github.com>
2022-09-03 15:14:09 +0530

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ Webcam.attach('#camera');
 function take_snapshot()
 {
     Webcam.snap(function(data_uri) {
-        document.getElementById("result").inerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
     })
 }
 
